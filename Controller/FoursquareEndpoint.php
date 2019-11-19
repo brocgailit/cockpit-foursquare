412ffd21ac3c97b3d8dd65d45b6d9d74ac78ef42
Broc Gailit <brocgailit@gmail.com>
2019-11-19 15:08:50 -0800

    Use named array?

--- Controller/FoursquareEndpoint.php
+++ Controller/FoursquareEndpoint.php
@@ -10,15 +10,14 @@ class FoursquareEndpoint {
 	private $client;
 
 	public function __construct($base_uri, $options) {
-		$this->client_id = $options->client_id;
-		$this->client_secret = $options->$client_secret;
+		$this->client_id = $options['client_id'];
+		$this->client_secret = $options['client_secret'];
 		$this->client = new Client([
 			'base_uri' => $base_uri
 		]);
 	}
 
 	public function query($endpoint = '', $options = []) {
-		return '$this->client_id';
 		$res = $this->client->request('GET', $endpoint, [
 			'query' => array_merge([
 				'v' => '20190112',
